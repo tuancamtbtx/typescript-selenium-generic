5a9ee91e6e53b897f9c5bf9ada20d9ce0844420b
tuancamtbtx <nguyenvantuan140397@gmail.com>
2024-08-17 13:39:11 +0700

    [add] selector spec

---
+++ src/spec/page.ts
@@ -0,0 +1,6 @@
+export default class PageSpec {
+    step: number
+    page_name: string
+    url: string
+    tasks: any
+}
---
+++ src/spec/selectors.ts
@@ -0,0 +1,7 @@
+export default class SelectorSpec {
+    step: number
+    type_selector: string
+    element_selector: string
+    action: string
+    value: string
+}
