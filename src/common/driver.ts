5b5eb4ba03f4d2e97bc123d9ca5125ce56ab8be5
tuancamtbtx <nguyenvantuan140397@gmail.com>
2024-08-19 11:42:15 +0700

    [add] drive selenium

---
+++ src/common/driver.ts
@@ -0,0 +1,9 @@
+export default class SeleniumDriver {
+    public async start() {
+        // Start the selenium driver
+    }
+    
+    public async stop() {
+        // Stop the selenium driver
+    }
+}
