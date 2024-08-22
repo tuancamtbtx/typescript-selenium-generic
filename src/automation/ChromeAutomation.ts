7765bcf769b50ab115ff607ea9f4e89d65baadf9
tuancamtbtx <nguyenvantuan140397@gmail.com>
2024-08-22 22:03:15 +0700

    [add] chrome automation

---
+++ src/automation/ChromeAutomation.ts
@@ -0,0 +1,14 @@
+export default class ChromeAutomation {
+    public async start() {
+        // Start the chrome automation
+    }
+    public async getDriver() {
+        // Get the chrome automation driver
+    }
+    public async stop() {
+        // Stop the chrome automation
+    }
+    public async execute() {
+        // Execute the chrome automation
+    }
+}
