86b2113a995ef3fd846e483387a618f3e1e7446b
tuancamtbtx <nguyenvantuan140397@gmail.com>
2024-10-05 00:32:15 +0700

    [add] new driver

--- src/common/driver.ts
+++ src/common/driver.ts
@@ -3,14 +3,18 @@ import { Builder, Capabilities, WebDriver } from 'selenium-webdriver';
 
 
 export default class SeleniumDriver {
+    driver: WebDriver;
     public async start() {
         // Start the selenium driver
     }
-    public async getDriver() {
+    public async getDriver(options:[key:string]) : Promise<WebDriver> {
         // Get the selenium driver
-        const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
+        const driver = new Builder()
+        .options(options)
+        .withCapabilities(Capabilities.chrome()).build();
+        return driver;
     }
     public async stop() {
-        // Stop the selenium driver
+        this.driver.quit();
     }
 }
